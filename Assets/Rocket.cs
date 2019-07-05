42be1f31448fe1bd3c0d357a297bca14972493b2
Ricardo Vargas <r.sagrero1@gmail.com>
2019-07-05 01:50:36 -0500

    we added the audio to the trusters using the AudioSource

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    //In this program we will proces input
+    Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*the <> arrows will ther the method to access only to the RigidBody Component */
+        rigidBody = GetComponent<Rigidbody>();//Provided by Unity
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        //audioSource.Stop();
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        else
+        {
+            print("Nothing is being pressed");
+        }
+
     }
 }
