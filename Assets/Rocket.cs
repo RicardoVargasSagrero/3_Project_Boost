9114ffc8c2f93c0032eae36aa8c6848d7d4c9539
Ricardo Vargas <r.sagrero1@gmail.com>
2019-07-09 18:20:48 -0500

    We added particles to the thrust, death and success states

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,12 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     //In this program we will proces input
+    
+    //With the [SerializeField] we're able to see the variable in the Unity Inspector
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 25f;
+    //Audio resourses
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+    //Particle System Resource
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    //States of the game
+    enum State { Alive,Dying,Transcending};
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,33 +38,100 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        // todo Stop the death sound
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        else if (state == State.Transcending)
+        {
+            //Do nothing
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default: //What ever is untagged it will be the default option
+                StartDeathSequence();
+                break;
+        } 
     }
 
-    private void ProcessInput()
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        print("Obstacle");
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", 5f);
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        print("Hit finish");
+        Invoke("LoadNextLevel", 5f);
+
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying)
-                audioSource.Play();
+            ApplyThrust();
         }
         else
-        {
+         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
-        //audioSource.Stop();
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(mainEngine);
+        mainEngineParticles.Play();
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         else
         {
-            print("Nothing is being pressed");
+            //print("Nothing is being pressed");
         }
-
+        rigidBody.freezeRotation = false; //resume physicss control of rotation
     }
+
+    
 }
